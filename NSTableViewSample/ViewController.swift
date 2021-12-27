4be41859e1f6410e6e614044344e84d977cb1d37
Danny Shen <shenfive@hotmail.com>
2021-12-27 15:59:24 +0800

    Initial Commit

---
+++ NSTableViewSample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NSTableViewSample
+//
+//  Created by 申潤五 on 2021/12/27.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
